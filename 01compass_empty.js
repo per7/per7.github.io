d5a7818630a16b39b4d07eed959db0cfc2cf621b
per7 <per.ehrstrom@gmail.com>
2018-02-22 12:06:47 +0200

    added if statements

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,9 +8,26 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
-         text_area.textContent = compassHeading;
+
+        if(compassHeading > 315 || compassHeading < 45) {
+                text_area.textContent = "the sea";
+        }
+
+        if(compassHeading >= 45 && compassHeading < 135) {
+                text_area.textContent = "a nightclub";
+        }
+
+        if(compassHeading >= 135 && compassHeading < 225) {
+                text_area.textContent = "the forest";
+        }
+
+        if(compassHeading >= 225 && compassHeading < 315) {
+                text_area.textContent = "the sea";
+        }
+        
+        // text_area.textContent = compassHeading;
         //calls function to alter content based on heading
-       // myOrientation();
+        // myOrientation();
     }
 
 //device orientation that can track orientation on space
