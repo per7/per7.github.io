9e6748cbe3cae64cdee8a8b6d23df8734ec1731c
per7 <per.ehrstrom@gmail.com>
2018-02-23 08:05:49 +0200

    ekpraksis

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,22 +8,67 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
-
-        if(compassHeading > 315 || compassHeading < 45) {
-                text_area.textContent = "the frozen sea";
+        if(compassHeading > 315 || compassHeading < 20) {
+                text_area.textContent = "Was he a part of that group? I cant remember. ";
         }
 
-        if(compassHeading >= 45 && compassHeading < 135) {
-                text_area.textContent = "a man, <br/> drinking water";
+        if(compassHeading >= 40 && compassHeading < 60) {
+                text_area.textContent = "I think he was. He’s on the opposite side of the room. ";
         }
 
-        if(compassHeading >= 135 && compassHeading < 225) {
-                text_area.textContent = "turning water <br/> into a tear";
+        if(compassHeading >= 60 && compassHeading < 80) {
+                text_area.textContent = "He’s watching, writing. I cant tell if he is actively taking part or not.";
         }
 
-        if(compassHeading >= 225 && compassHeading < 315) {
-                text_area.textContent = "tasting <br/> salt water";
+        if(compassHeading >= 80 && compassHeading < 100) {
+                text_area.textContent = "Looks like he is writing what he is seeing. Like I am writing what I am seeing, only I’m not seeing much of what the rest of the group is doing. ";
         }
+        
+        if(compassHeading > 100 || compassHeading < 120) {
+                text_area.textContent = "Or rather, I do see what they are doing and I understand what it is they are doing, maybe Im not very interested in it.";
+        }
+
+        if(compassHeading >= 120 && compassHeading < 140) {
+                text_area.textContent = "Probably because I understand it. I don’t really understand what he is doing. I move closer, first to the north side of the room. ";
+        }
+
+        if(compassHeading >= 140 && compassHeading < 160) {
+                text_area.textContent = ". I stay there for a while, as if Im looking at what the rest of the group is doing, but really Im trying to get closer to him. ";
+        }
+
+        if(compassHeading >= 160 && compassHeading < 180) {
+        
+                text_area.textContent = "From this angle I see him writing in his notebook, he probably is writing what they are doing. ";
+        }
+
+        if(compassHeading > 180 || compassHeading < 200) {
+                text_area.textContent = ". If he is, will he use it? If he is going to use it in some way, he needs to be quick.";
+        }
+
+        if(compassHeading >= 200 && compassHeading < 220) {
+                text_area.textContent = "My inner clock tells me they have about one minute left, but I cant be sure. I move closer, I want to peek behind his shoulder to see what he has written";
+        }
+
+        if(compassHeading >= 220 && compassHeading < 240) {
+                text_area.textContent = " I remember sitting quite closely to where he is sitting now.";
+        }
+
+        if(compassHeading >= 240 && compassHeading < 260) {
+                text_area.textContent = "I remember watching a group of people in the middle of the room. I remember writing what I was seing";
+        }
+        
+        if(compassHeading > 260 || compassHeading < 280) {
+                text_area.textContent = "The time must be up soon. Maybe he will say something just as the time is up, is he aware of the time?";
+        }
+
+        if(compassHeading >= 280 && compassHeading < 300) {
+                text_area.textContent = "I dont see him looking at a clock or phone. The time is up. He doesnt say anything.";
+        }
+
+        if(compassHeading >= 300 && compassHeading < 315) {
+                text_area.textContent = "He doesnt show the text to anyone. I cant see anything of what he has written.";
+        }
+
         
         // text_area.textContent = compassHeading;
         //calls function to alter content based on heading
